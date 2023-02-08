3e8bf3b1ed8997b3c38102467507c7d06cfde0d0
shahamisha <shahamisha012@gmail.com>
2023-02-08 12:21:53 +0530

    commmit

--- javascript.js
+++
@@ -1,7 +0,0 @@
-<script>
-  <button onclick="document.getElementById('img').src='on.jpg'">on</button>
-
-  <img id="img" src="off.jpg" style="width: 100px" />
-
-  <button onclick="document.getElementById('img').src='off.jpg'">off</button>
-</script>;
