28f3f3e170bfa5abb37b2244279ae68f80f9bf94
shahamisha <shahamisha012@gmail.com>
2023-02-10 19:50:23 +0530

    HTML template

--- javascript.js
+++ javascript.js
@@ -978,6 +978,35 @@ Output: Hello world! */
 
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+//HTML Templates
+
+{
+  /*let header = "Subjects";
+let tags = ["css", "javascript", "es6"];
+
+let html = `<h2>${header}</h2><ul>`;
+
+for (const x of tags) {
+  html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
+document.getElementById("demo").innerHTML = html;
+Output: Subjects
+css
+javascript
+es6 */
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//Quotes Inside Strings : (With template literals, you can use both single and double quotes inside a string:)
+{
+  /* let text = `He's often called "Johnny"`;
+document.getElementById("demo").innerHTML = text;
+Output: He's often called "Johnny" */
+}
+
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*---------------------------------------------------------------------------------*/
